8c13463f516b0c72b973ed5054a8e8738362ab0c
Penelope Gomez / Pogmommy <support@pogmom.me>
2023-05-18 14:56:31 -0600

    check if running before attempting to kill check if launchagent is loaded & remove

--- scripts/uninstall-macos.sh
+++ scripts/uninstall-macos.sh
@@ -1,6 +1,11 @@
 #!/bin/zsh
 # A script to autoamtically uninstall jellyfin-rpc on mac
-killall jellyfin-rpc #stop any running processes
+if pgrep -xq -- "jellyfin-rpc"; then
+    killall jellyfin-rpc # Kill jellyfin-rpc if it is running
+fi
+if launchctl list | grep Jellyfin-RPC; then
+    launchctl remove Jellyfin-RPC # Unload Jellyfin-RPC launchagent if it is loaded
+fi
 rm ~/Library/LaunchAgents/jellyfinrpc.local.plist #remove launch agent
 rm -rf ~/.config/jellyfin-rpc #remove config file
 rm /usr/local/bin/jellyfin-rpc #remove binary
